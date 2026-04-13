f43ab6f194917920a3d9cfe773e2896d4d8fe02c
lucasaleduarte <lucasaleduarte0@gmail.com>
2026-04-13 11:25:23 -0300

    Terminando as funções

--- modulo/busca_contatos.js
+++ modulo/busca_contatos.js
@@ -21,8 +21,6 @@ const getListaDadosUsuario = function(){
 // (Todos os dados do profile que podem ser alterados como nome,“nick”,
 // foto, número, imagem, cor de fundo e dados da conta como criação e
 // encerramento, etc)
-
-
 const getListaDadosProfile = function(numero){
 
     if(!numero) return false
@@ -68,7 +66,7 @@ const getListaDadosProfile = function(numero){
     return false
  }
 
- console.log(getListaDadosContato('11987876567'))
+ //console.log(getListaDadosContato('11987876567'))
 
 
 //  const getListaDadosContato = function(numero){
@@ -90,5 +88,96 @@ const getListaDadosProfile = function(numero){
 //         }
 //     })
 //     return listaContatos
-    
 //  }
+
+
+//Listar todas as mensagens trocadas de uma conta de usuário
+//(Retornar todos os dados)
+
+const getTodasMensagensUsuario = function(numeroUsuario){
+    let listaMensagens = false
+
+    listaDeContatos.contatos['whats-users'].forEach(function(usuario){
+        if (usuario.number == numeroUsuario){
+            listaMensagens = {
+                nomeUsuario: usuario.account,
+                contatos: []
+            }
+            usuario.contacts.forEach(function(contato){
+                let conversa = {
+                    contato: contato.name,
+                    mensagens: contato.messages
+                }
+
+                listaMensagens.contatos.push(conversa)
+            })
+        }
+    })
+
+    return listaMensagens
+}
+
+//console.log(getTodasMensagensUsuario("11987876567"))
+
+// Listar uma conversa de um usuário e um contato
+// (Retornar dados como: nome, número de celular e as
+// conversas). Deve obrigatoriamente encaminhar a referência
+// para encontrar a conversa via Query e não via parâmetro)
+
+const getConversasUsuario = function(numero, nomeContato){
+
+    const usuario = listaDeContatos.contatos['whats-users'].find(function(item){
+        return item.number == numero
+    })
+
+    if (usuario){
+        const contato = usuario.contacts.find(function(c){
+            return c.name.toLowerCase() == nomeContato.toLowerCase()
+        })
+
+        if(contato){
+            return{
+                nome: usuario.account,
+                numero: usuario.number,
+                contato_nome: contato.name,
+                conversas: contato.messages
+            }
+        }
+    }
+    return false
+}
+
+//console.log(getConversasUsuario("11987876567", "Ana Maria"))
+
+// Realizar um filtro como “pesquisa de palavra chave” com
+// base em uma palavra nas conversas do usuário e do
+// respectivo contato
+
+const getMensagensFiltradas = function(numeroUsuario, nomeContato, palavraChave) {
+    // 1. Localiza o usuário dono da conta [2, 3]
+    const usuario = listaDeContatos.contatos['whats-users'].find(item => item.number == numeroUsuario)
+
+    if (usuario) {
+        // 2. Localiza o contato específico na lista do usuário [2]
+        const contato = usuario.contacts.find(c => c.name.toLowerCase() == nomeContato.toLowerCase())
+
+        if (contato) {
+            // 3. Filtra as mensagens que contêm a palavra-chave [1]
+            // Usamos .toLowerCase() para tornar a busca insensível a maiúsculas [4]
+            const mensagensFiltradas = contato.messages.filter(function(mensagem) {
+                return mensagem.content.toLowerCase().includes(palavraChave.toLowerCase())
+            })
+
+            // 4. Retorna o objeto formatado com os dados solicitados [5]
+            return {
+                usuario: usuario.account,        
+                contato: contato.name,
+                palavra_pesquisada: palavraChave,
+                quantidade: mensagensFiltradas.length,
+                mensagens: mensagensFiltradas
+            }
+        }
+    }
+    return false
+}
+console.log(getMensagensFiltradas("11955577796", "Peter Wilsen",  "bem"))
